a77650fda406a08a1b84f2efca3459f8d2c23ac2
Washington Santos <washingtonstk@gmail.com>
2018-09-25 00:22:54 -0300

    add new functions to implement

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,26 +1,14 @@
 module Lib( 
-    polinomio2
-    , isTriangle
-    , mean
+    mean
     , median
+--    , mode
+--    , variance
+--    , stdev
+--    , variance_
+--    , stdev_
 ) where
 
 import Data.List
-
-delta :: (Num a) => a -> a -> a -> a
-delta a b c = (b^2) - (4 * a * c)
-
-calc :: (Floating b) => b -> b -> b -> b -> (b, b)
-calc a b c d = (((-b) + sqrt(d))/(2*a), ((-b) - sqrt(d))/(2*a))
-
-polinomio2 :: (Ord b, Floating b) => b -> b -> b -> (b, b)
-polinomio2 a b c = if a == 0 || d < 0 then (-1,-1) else calc a b c d where d = delta a b c
-
-isTriangle :: (Ord b, Floating b) => b -> b -> b -> Bool
-isTriangle a b c = if a < 0 || b < 0 || c < 0 then 
-                        False 
-                   else
-                        b + c >= a && c + a >= b && a + b >= c
 
 mean :: (Fractional a) => [a] -> a
 mean xs = (sum xs)/fromIntegral(length xs)
@@ -34,4 +22,23 @@ median xs = do
                             let m = ((length s) `div` 2)
                             mean [s !! m, s !! (m-1)]
             where s = sort xs
+
+-- mode :: (Ord a, Fractional a) => [a] -> a
+
+-- population
+-- variance :: (Ord a, Fractional a) => [a] -> a
+
+-- population
+-- stdev :: (Ord a, Fractional a) => [a] -> a
+
+-- sample
+-- variance_ :: (Ord a, Fractional a) => [a] -> a
+
+-- sample
+-- stdev_ :: (Ord a, Fractional a) => [a] -> a
+
+-- percentile :: (Ord a, Fractional a) => [a] -> a -> a
+
+-- quartile :: (Ord a, Fractional a) => [a] -> a -> a
+
 
